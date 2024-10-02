3af111bfe8c0f5a8f552f989512c234f99420417
박인재 <qkrdlswo98@gmail.com>
2024-10-03 00:39:40 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+//사칙연산
+int main() {
+  int a, b;
+  
+  cin >> a >> b;
+  
+  cout 
+  << a + b << endl
+  << a - b << endl
+  << a * b << endl
+  << a / b << endl
+  << a % b;
+  
+  return 0;
+}
