657308b846367cf4fbf20d7642feae6179660ad9
박인재 <qkrdlswo98@gmail.com>
2024-10-03 00:46:56 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2052 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+
+// 18108
+int main() {
+  int year;
+  cin >> year;
+  
+  cout << year - 543;
+  
+  return 0;
+}
+
+//// 10926
+//int main() {
+//  string name;
+//  cin >> name;
+//  
+//  cout << name << "??!";
+//  
+//  return 0;
+//}
+
+//// 10869
+//int main() {
+//  int a, b;
+//  
+//  cin >> a >> b;
+//  
+//  cout 
+//  << a + b << endl
+//  << a - b << endl
+//  << a * b << endl
+//  << a / b << endl
+//  << a % b;
+//  
+//  return 0;
+//}
+
+//// 1008 -> 블로그
+//int main() {
+//  int a, b;
+//  
+//  cin >> a >> b;
+//  cout.precision(50);
+////  cout << fixed;
+//  
+//  cout << (double) a / b;
+//  
+//  return 0;
+//}
+
+//// 10998
+//int main() {
+//  int a, b;
+//  
+//  cin >> a >> b;
+//  
+//  cout << a * b;
+//  
+//  return 0;
+//}
+
+//// 1001
+//int main() {
+//  int a, b;
+//  
+//  cin >> a >> b;
+//  
+//  cout << a - b;
+//  
+//  return 0;
+//}
+
+//// 1000
+//int main() {
+//  int a, b;
+//  
+//  cin >> a >> b;
+//  
+//  cout << a + b;
+//  
+//  return 0;
+//}
+
+//// 2557
+//int main() {
+//  cout << "Hello World!";
+//
+//  return 0;
+//}
