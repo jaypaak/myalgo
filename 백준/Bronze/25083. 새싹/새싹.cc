88aa94c853b986a3a8d447379ddd137b3d89e41a
박인재 <qkrdlswo98@gmail.com>
2024-10-03 00:47:06 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// 25083
+int main() {
+  cout << "         ,r'\"7" << "\n";
+  cout << "r`-_   ,'  ,/" << "\n";
+  cout << " \\. \". L_r'" << "\n";
+  cout << "   `~\\/" << "\n";
+  cout << "      |" << "\n";
+  cout << "      |" << "\n";
+  
+  return 0;
+}
