0aa9af7f9f7d2deab29b19e6d2ccfed0f73dcb55
박인재 <qkrdlswo98@gmail.com>
2024-10-03 00:47:32 +0900

    [Bronze IV] Title: 영수증, Time: 0 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.cc
@@ -0,0 +1,89 @@
+#include <iostream>
+using namespace std;
+
+// 10951
+
+
+
+// 10952
+
+
+
+// 2439
+
+
+
+// 2438
+
+
+
+// 11022
+
+
+
+// 11021
+
+
+
+// 15552
+
+
+
+// 25314
+
+
+
+// 25304
+int main() {
+  int total, n1, sum = 0;
+  cin >> total;
+  cin >> n1;
+  for (int i = 0; i < n1; i++) {
+    int price, n2;
+    cin >> price >> n2;
+    sum += price * n2;
+  }
+  
+  string r = (total == sum) ? "Yes" : "No";
+  
+  cout << r;
+  
+  return 0;
+}
+
+
+//// 8393
+//int main() {
+//  int n, sum = 0;
+//  cin >> n;
+//  
+//  for (int i = 1; i <= n; i++) sum += i;
+//  
+//  cout << sum;
+//  
+//  return 0;
+//}
+
+//// 10950
+//int main() {
+//  short a, b, t;
+//  cin >> t;
+//  for (int i  = 0; i < t; i++) {
+//    cin >> a >> b;
+//    cout << a + b << endl;
+//  }
+//  
+//  return 0;
+//}
+
+//// 2793
+//int main() {
+//  short n;
+//  cin >> n;
+//  
+//  for (int i = 1; i < 10; i++)
+//    cout << n << " * " << i << " = " << n * i << endl;
+//  
+//  
+//  return 0;
+//}
