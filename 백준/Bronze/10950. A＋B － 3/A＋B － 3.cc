f692f4ddd05979b38fa0c06932192e272df98c46
박인재 <qkrdlswo98@gmail.com>
2024-10-03 00:40:32 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+//
+
+
+
+// 10951
+
+
+
+// 10952
+
+
+
+// 2439
+
+
+
+// 2438
+
+
+
+// 11022
+
+
+
+// 11021
+
+
+
+// 15552
+
+
+
+// 25314
+
+
+
+// 25304
+
+
+
+// 8393
+
+
+
+// 10950
+int main() {
+  short a, b, t;
+  cin >> t;
+  for (int i  = 0; i < t; i++) {
+    cin >> a >> b;
+    cout << a + b << endl;
+  }
+  
+  
+  return 0;
+}
+
+//// 2793
+//int main() {
+//  short n;
+//  cin >> n;
+//  
+//  for (int i = 1; i < 10; i++)
+//    cout << n << " * " << i << " = " << n * i << endl;
+//  
+//  
+//  return 0;
+//}
