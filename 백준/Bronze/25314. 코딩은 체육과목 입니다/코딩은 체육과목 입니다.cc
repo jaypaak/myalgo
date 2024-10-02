b8a195b620f76eb336070f1c3aed76cfccad0ca2
박인재 <qkrdlswo98@gmail.com>
2024-10-03 00:47:43 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2052 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,94 @@
+#include <iostream>
+using namespace std;
+
+// 10951
+
+
+
+// 10952
+
+
+
+// 2439
+
+
+
+// 2438
+
+
+
+// 11022
+
+
+
+// 11021
+
+
+
+// 15552
+
+
+
+// 25314
+int main() {
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; i += 4) cout << "long ";
+  cout << "int";
+  
+  return 0;
+}
+
+//// 25304
+//int main() {
+//  int total, n1, sum = 0;
+//  cin >> total;
+//  cin >> n1;
+//  for (int i = 0; i < n1; i++) {
+//    int price, n2;
+//    cin >> price >> n2;
+//    sum += price * n2;
+//  }
+//  
+//  string r = (total == sum) ? "Yes" : "No";
+//  
+//  cout << r;
+//  
+//  return 0;
+//}
+
+//// 8393
+//int main() {
+//  int n, sum = 0;
+//  cin >> n;
+//  
+//  for (int i = 1; i <= n; i++) sum += i;
+//  
+//  cout << sum;
+//  
+//  return 0;
+//}
+
+//// 10950
+//int main() {
+//  short a, b, t;
+//  cin >> t;
+//  for (int i  = 0; i < t; i++) {
+//    cin >> a >> b;
+//    cout << a + b << endl;
+//  }
+//  
+//  return 0;
+//}
+
+//// 2793
+//int main() {
+//  short n;
+//  cin >> n;
+//  
+//  for (int i = 1; i < 10; i++)
+//    cout << n << " * " << i << " = " << n * i << endl;
+//  
+//  
+//  return 0;
+//}
