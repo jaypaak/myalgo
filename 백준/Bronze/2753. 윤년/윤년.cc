a83b1ad3d723877c5842eef4d6b6e55ffc482197
박인재 <qkrdlswo98@gmail.com>
2024-10-03 00:27:43 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2052 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+// 2753
+int main() {
+  int y;
+  cin >> y;
+  
+  if (((y % 100) != 0 || (y % 400) == 0) && ((y % 4) == 0)) {
+    cout << 1;
+  } else {
+    cout << 0;
+  }
+  
+  return 0;
+}
+
+//// 9498
+//int main() {
+//  int score;
+//  cin >> score;
+//  
+//  char r = (score >= 90) ? 'A'
+//  : (score >= 80) ? 'B'
+//  : (score >= 70) ? 'C'
+//  : (score >= 60) ? 'D'
+//  : 'F';
+//  
+//  cout << r;
+//  
+//  return 0;
+//}
+
+//// 1330
+//int main() {
+//  int a, b;
+//  cin >> a >> b;
+//  
+//  string r = (a < b) ? "<"
+//  : (a > b) ? ">"
+//  : "==";
+//  
+//  cout << r;
+//  
+//  return 0;
+//}
