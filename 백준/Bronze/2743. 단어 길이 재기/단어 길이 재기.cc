d0c15a046268117a04035dd3a3a6060fb71b7c28
박인재 <qkrdlswo98@gmail.com>
2024-10-03 00:26:58 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+// 2743
+int main() {
+  string s;
+  cin >> s;
+  
+  cout << s.size();
+  
+  return 0;
+}
+
+//// 27866
+//int main() {
+//  string s;
+//  int idx;
+//  cin >> s;
+//  cin >> idx;
+//  
+//  cout << s.at(idx - 1);
+//  
+//  return 0;
+//}
