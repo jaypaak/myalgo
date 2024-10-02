323cd408a9d3b9e428ce635d34946898fbc64486
박인재 <qkrdlswo98@gmail.com>
2024-10-03 00:43:55 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2052 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+// 11654
+int main() {
+  char c;
+  cin >> c;
+  
+  cout << (int) c;
+  
+  return 0;
+}
+
+//// 9086
+//int main() {
+//  int n;
+//  cin >> n;
+//  
+//  for (int i = 0; i < n; i++) {
+//    string s;
+//    cin >> s;
+//    
+//    cout << s.at(0) << s.at(s.size() - 1) << "\n";
+//  }
+//  
+//  return 0;
+//}
+
+//// 2743
+//int main() {
+//  string s;
+//  cin >> s;
+//  
+//  cout << s.size();
+//  
+//  return 0;
+//}
+
+//// 27866
+//int main() {
+//  string s;
+//  int idx;
+//  cin >> s;
+//  cin >> idx;
+//  
+//  cout << s.at(idx - 1);
+//  
+//  return 0;
+//}
